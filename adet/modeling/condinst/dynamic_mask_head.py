5a65be3db8219f3ad633ca6d871b25d9d4d1d526
Jihang Zhang <jihangz2519@gmail.com>
2020-08-31 11:17:28 -0700

    Remove Lovasz

--- adet/modeling/condinst/dynamic_mask_head.py
+++ adet/modeling/condinst/dynamic_mask_head.py
@@ -1,5 +1,4 @@
 import torch
-from torch.autograd import Variable
 from torch.nn import functional as F
 from torch import nn
 
@@ -7,7 +6,6 @@ from adet.utils.comm import compute_locations, aligned_bilinear
 
 
 def dice_coefficient(x, target):
-    x = x.sigmoid()
     eps = 1e-5
     n_inst = x.size(0)
     x = x.reshape(n_inst, -1)
@@ -16,97 +14,6 @@ def dice_coefficient(x, target):
     union = (x ** 2.0).sum(dim=1) + (target ** 2.0).sum(dim=1) + eps
     loss = 1. - (2 * intersection / union)
     return loss
-
-def lovasz_grad(gt_sorted):
-    """
-    Computes gradient of the Lovasz extension w.r.t sorted errors
-    See Alg. 1 in paper
-    """
-    p = len(gt_sorted)
-    gts = gt_sorted.sum()
-    intersection = gts - gt_sorted.float().cumsum(0)
-    union = gts + (1 - gt_sorted).float().cumsum(0)
-    jaccard = 1. - intersection / union
-    if p > 1: # cover 1-pixel case
-        jaccard[1:p] = jaccard[1:p] - jaccard[0:-1]
-    return jaccard
-
-def symmetric_lovasz(outputs, targets):
-        return (lovasz_hinge(outputs, targets) + lovasz_hinge(-outputs, 1 - targets)) / 2
-
-def lovasz_hinge(logits, labels, per_image=True, ignore=None):
-    """
-    Binary Lovasz hinge loss
-      logits: [B, H, W] Variable, logits at each pixel (between -\infty and +\infty)
-      labels: [B, H, W] Tensor, binary ground truth masks (0 or 1)
-      per_image: compute the loss per image instead of per batch
-      ignore: void class id
-    """
-    if per_image:
-        loss = torch.tensor([lovasz_hinge_flat(*flatten_binary_scores(log.unsqueeze(0), lab.unsqueeze(0), ignore))
-                          for log, lab in zip(logits, labels)])
-    else:
-        loss = lovasz_hinge_flat(*flatten_binary_scores(logits, labels, ignore))
-    return loss
-
-
-def lovasz_hinge_flat(logits, labels):
-    """
-    Binary Lovasz hinge loss
-      logits: [P] Variable, logits at each prediction (between -\infty and +\infty)
-      labels: [P] Tensor, binary ground truth labels (0 or 1)
-      ignore: label to ignore
-    """
-    if len(labels) == 0:
-        # only void pixels, the gradients should be 0
-        return logits.sum() * 0.
-    signs = 2. * labels.float() - 1.
-    errors = (1. - logits * Variable(signs))
-    errors_sorted, perm = torch.sort(errors, dim=0, descending=True)
-    perm = perm.data
-    gt_sorted = labels[perm]
-    grad = lovasz_grad(gt_sorted)
-    loss = torch.dot(F.elu(errors_sorted) + 1, Variable(grad))
-    return loss
-
-
-def flatten_binary_scores(scores, labels, ignore=None):
-    """
-    Flattens predictions in the batch (binary case)
-    Remove labels equal to 'ignore'
-    """
-    scores = scores.flatten()
-    labels = labels.flatten()
-    if ignore is None:
-        return scores, labels
-    valid = (labels != ignore)
-    vscores = scores[valid]
-    vlabels = labels[valid]
-    return vscores, vlabels
-
-def isnan(x):
-    return x != x
-    
-    
-def mean(l, ignore_nan=False, empty=0):
-    """
-    nanmean compatible with generators.
-    """
-    l = iter(l)
-    if ignore_nan:
-        l = ifilterfalse(isnan, l)
-    try:
-        n = 1
-        acc = next(l)
-    except StopIteration:
-        if empty == 'raise':
-            raise ValueError('Empty mean')
-        return empty
-    for n, v in enumerate(l, 2):
-        acc += v
-    if n == 1:
-        return acc
-    return acc / n
 
 
 def parse_dynamic_params(params, channels, weight_nums, bias_nums):
@@ -171,7 +78,6 @@ class DynamicMaskHead(nn.Module):
         self.weight_nums = weight_nums
         self.bias_nums = bias_nums
         self.num_gen_params = sum(weight_nums) + sum(bias_nums)
-        self.mask_loss = symmetric_lovasz if cfg.MODEL.CONDINST.MASK_HEAD.LOVASZ else dice_coefficient
 
     def mask_heads_forward(self, features, weights, biases, num_insts):
         '''
@@ -236,7 +142,7 @@ class DynamicMaskHead(nn.Module):
         assert mask_feat_stride % self.mask_out_stride == 0
         mask_logits = aligned_bilinear(mask_logits, int(mask_feat_stride / self.mask_out_stride))
 
-        return mask_logits
+        return mask_logits.sigmoid()
 
     def __call__(self, mask_feats, mask_feat_stride, pred_instances, gt_instances=None):
         if self.training:
@@ -247,19 +153,18 @@ class DynamicMaskHead(nn.Module):
             if len(pred_instances) == 0:
                 loss_mask = mask_feats.sum() * 0 + pred_instances.mask_head_params.sum() * 0
             else:
-                mask_logits = self.mask_heads_forward_with_coords(
+                mask_scores = self.mask_heads_forward_with_coords(
                     mask_feats, mask_feat_stride, pred_instances
                 )
-                mask_losses = self.mask_loss(mask_logits, gt_bitmasks)
+                mask_losses = dice_coefficient(mask_scores, gt_bitmasks)
                 loss_mask = mask_losses.mean()
 
             return loss_mask.float()
         else:
             if len(pred_instances) > 0:
-                mask_logits = self.mask_heads_forward_with_coords(
+                mask_scores = self.mask_heads_forward_with_coords(
                     mask_feats, mask_feat_stride, pred_instances
                 )
-                mask_scores = mask_logits.sigmoid()
                 pred_instances.pred_global_masks = mask_scores.float()
 
             return pred_instances
